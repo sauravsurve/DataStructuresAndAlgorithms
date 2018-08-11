851dd154f418390e71c3846252ac0ff969308751
Kevin Vaz <36708857+excalibur-kvrv@users.noreply.github.com>
2018-08-11 15:37:19 +0530

    Correct error
    this corrects the error of invalid error on line 19

--- fastScanner.java
+++ fastScanner.java
@@ -16,7 +16,7 @@ class fastScanner {
 		}
 
 		String next() {
-			while(st==null||!st.hasNextTokens()) {
+			while(st==null||!st.hasMoreTokens()) {
 				try {
 					st = new StringTokenizer(br.readLine());
 				} catch(IOException e) {
